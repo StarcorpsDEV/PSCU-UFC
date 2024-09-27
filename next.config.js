1132afee9360edb9c72c1d5f3290220d3e2f94af
StarsorpsDEV <starcorps@protonmail.com>
2024-09-27 20:36:40 +0300

    commit 2

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
-require("dotenv").config()
+require('dotenv').config();
 
 module.exports = {
-env: {
-  REACT_APP_PRIVATE_KEY: process.env.REACT_APP_PRIVATE_KEY,
-  REACT_APP_ALCHEMY_API_URL: process.env.REACT_APP_ALCHEMY_API_URL,
-  REACT_APP_WALLET_ADDRESS: process.env.REACT_APP_WALLET_ADDRESS,
-  REACT_APP_DEFAUT_DAO_MEMBERS: process.env.REACT_APP_DEFAUT_DAO_MEMBERS,
-},
+  env: {
+    REACT_APP_PRIVATE_KEY: process.env.REACT_APP_PRIVATE_KEY,
+    REACT_APP_ALCHEMY_API_URL: process.env.REACT_APP_ALCHEMY_API_URL,
+    REACT_APP_WALLET_ADDRESS: process.env.REACT_APP_WALLET_ADDRESS,
+    NEXT_PUBLIC_THIRDWEB_CLIENT_ID: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
+    REACT_APP_DEFAUT_DAO_MEMBERS: process.env.REACT_APP_DEFAUT_DAO_MEMBERS,
+  },
   reactStrictMode: true,
   swcMinify: false,
   productionBrowserSourceMaps: true,
